60e72b508f6fd3a02956cc8895b60b65205c970e
Ivan Volkov <volkivanv@mail.ru>
2023-09-03 20:22:43 +0300

    Добавил первую задачу - максимальное из двух

---
+++ MaximumFromTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input a!");
+double a = 2;
+a = Double.Parse(Console.ReadLine());
+Console.WriteLine("Input b!");
+double b = 7; 
+b = Double.Parse(Console.ReadLine());
+if (a < b){ 
+    Console.WriteLine("Max number is b = " + b);
+}else{
+    Console.WriteLine("Max number is b = " + a);
+};
+
