db38420287d79ede96cbb4a995b0e5e879a90710
Ivan Volkov <volkivanv@mail.ru>
2023-09-03 21:13:53 +0300

    добавил задачу 3 - четное или нечетное число

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Please, input your number!");
+double number = double.Parse(Console.ReadLine());
+if (number % 2 == 0) Console.WriteLine("Number "+ number + " is even!"); 
+else Console.WriteLine("Number "+ number + " is odd!");
