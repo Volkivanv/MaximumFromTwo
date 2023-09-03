65561b20fc26c529625d0d161dd13e0e3389488c
Ivan Volkov <volkivanv@mail.ru>
2023-09-03 20:59:06 +0300

    Добавил вторую задачу - максимум из трех

---
+++ MaximumFromThree/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input first number!");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Input second number!");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Input third number!");
+double c = double.Parse(Console.ReadLine());
+
+double max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max) 
+{
+    max = c;
+}
+
+Console.WriteLine("Maximum is " + max);
